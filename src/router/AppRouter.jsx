563ffae300dc7c3cc76942196aa2ea428452d018
özgebayram <ozgebayram1234@gmail.com>
2023-05-08 15:23:25 +0300

    feat: add router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+import Main from "../pages/Main"
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import MovieDetail from "../pages/MovieDetail";
+
+//  route kullanacaksak ilk yapacagımız route yapısını browser router ile sarmalamak
+
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return <BrowserRouter>
+  {/* roteslar bizim switch yapılarımız */}
+  <Routes>
+    <Route path="/" element={<Main/>}/>
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/register" element={<Register/>}/>
+    <Route path="/details/:id" element={<MovieDetail/>}/>
+
+
+
+  </Routes>
+         </BrowserRouter>;
 };
 
 export default AppRouter;
